e790ee336909e2c05082ea24033121d79bb73b2d
Hong Zheng <hzheng@oneresume.com>
2022-04-19 07:31:44 +0900

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.pinger.football',
+  appName: 'pinger-football',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
