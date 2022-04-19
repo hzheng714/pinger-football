f0d46caead9034607fb5e5b3e02428465c57a78c
Hong Zheng <hzheng@oneresume.com>
2022-04-19 20:08:40 +0900

    Added apiHost and apiKey to environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  api: 'https://v3.football.api-sports.io'
+  apiHost: 'v3.football.api-sports.io',
+  apiPrefix: 'https://v3.football.api-sports.io',
+  apiKey: 'c3d20397cd4dc283f2c1a423e47dcc6c'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,9 @@
 
 export const environment = {
   production: false,
-  api: 'https://v3.football.api-sports.io'
+  apiHost: 'v3.football.api-sports.io',
+  apiPrefix: 'https://v3.football.api-sports.io',
+  apiKey: 'c3d20397cd4dc283f2c1a423e47dcc6c'
 };
 
 /*
