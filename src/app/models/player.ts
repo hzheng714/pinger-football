29657c23ca80331d570dce11fa72b269b3804a4a
Hong Zheng <hzheng@oneresume.com>
2022-04-20 16:00:01 +0900

    Player: Added more properties

--- src/app/models/player.ts
+++ src/app/models/player.ts
@@ -1,7 +1,12 @@
 export interface Player {
     id: number;
     name: string;
-    photot: string;
+    photo: string;
+    age: number;
+    nationality: string;
+    height: string;
+    weight: string;
+    injured: boolean;
 }
 
 export interface PlayerItem {
